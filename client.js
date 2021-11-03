bd42eb08f6163c785e06f39c8efe37b9b5dae055
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-11-03 15:33:11 -0400

    Implement constants

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
+const { IP, PORT } = require("./constants");
 const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   conn.on("data", (data) => {
